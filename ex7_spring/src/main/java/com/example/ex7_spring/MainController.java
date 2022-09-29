bc88ef0318d3c38a245d7800b62e2fe91d20d8e5
Teodor <teodor.t.todorov@outlook.com>
2022-09-29 14:19:56 +0300

    Add comment for a different approach to make HTTP request by using path variables

--- ex7_spring/src/main/java/com/example/ex7_spring/MainController.java
+++ ex7_spring/src/main/java/com/example/ex7_spring/MainController.java
@@ -3,13 +3,17 @@ package com.example.ex7_spring;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
-    @RequestMapping("/home")
-    public String home(Model page) {
-        page.addAttribute("username", "Keti");
-        page.addAttribute("color", "red");
+    @RequestMapping("/home")                              //@RequestMapping("/home/{color}")
+    public String home(
+            @RequestParam(required = false) String name, //@PathVariable String name http://localhost:8080/home/name
+            @RequestParam(required = false) String color,
+            Model page) {
+        page.addAttribute("username", name);
+        page.addAttribute("color", color);
         return "home.html";
     }
 }
